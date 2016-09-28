4e10be3fe8843b788175cb6bbe3da5ef12191428
drmartinlutherxing <drmartinlutherchang@gmail.com>
2016-09-28 15:47:33 -0700

    _addHead creates an iframe wrapped with username from the bot at the url specified; _rmHead removes this bots and reference in _heads; say issues postMessage to appropriate iframe based on update;

--- js/games/heads/ui/Display.js
+++ js/games/heads/ui/Display.js
@@ -1,33 +1,49 @@
-CT.require();
-
 games.heads.ui.Display = CT.Class({
 	"CLASSNAME": "games.heads.ui.Display",
-	_heads: [],
+	_heads: {},
+	_addHead: function(name) {
+		this._heads[name] = CT.dom.node([
+				CT.dom.node(name, "DIV", "centered"),
+				CT.dom.iframe("http://45.79.138.63:8082/game0.html",
+					"bot-iframe")
+			], "DIV", "head-wrapper");
+		this.headsNode.appendChild(this._heads[name]);
+		this._resizeHeads();
+	},
+	_rmHead: function() {
+		CT.dom.remove(this._heads[user.name]);
+		delete this._heads[user.name];
+		this._resizeHeads();
+	},
 	_resizeHeads: function() {
 		if (this._heads.length > 0) {
 			this.log("multiple heads");
 		}
 	},
 	say: function(obj) {
+		this.log("say", obj);
 		//obj.name head issued message to say obj.message
+		debugger;
+		var iframe = this._heads[obj.user].children[1],
+			msg = obj.message.data.message;
+		CT.dom.getDoc(iframe).postMessage(msg, "*");
 	},
 	update: function(u) {
 		this.log("update", u);
+		if (u.message.action == "say")
+			this.say(u);
 		//call say
-
 	},
 	join: function(user) {
 		this.log("join", user);
-		//create head
-		this._resizeHeads();
+		this._addHead(user);
 	},
 	leave: function(user) {
 		this.log("leave", user);
-		//remove head
-		this._resizeHeads();
+		this._rmHead(user.name);
 	},
 	_build: function() {
-		this.view.classList.add("display-background w1 h1");
+		this.view.classList.add("display-background");
 		CT.dom.addContent(this.view,
 			CT.dom.node([
 					CT.dom.node("HEADS", "DIV", "centered"),
